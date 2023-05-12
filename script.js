706116bc050a83d7dee6ac421281178ec1241849
Demonthief <demon.thief16@gmail.com>
2023-05-12 20:16:18 +0700

    membuat section home

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const menu = document.querySelector('#bars'),
-    navbar = document.querySelector('.navbar')
+    navbar = document.querySelector('.navbar'),
+    navLinks = document.querySelectorAll('.navbar a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', () =>{
+            for(let i=0; i < navLinks.length; i++){
+                navLinks[i].classList.remove('active')
+            }
+            link.classList.add('active')
+        })
+    })
+    
 
 menu.addEventListener('click', () =>{
     menu.classList.toggle('fa-times')
@@ -22,3 +33,18 @@ const close = document.querySelector('#close')
 close.addEventListener('click', () =>{
     searchForm.classList.remove('active')
 })
+
+// swipper
+var swiper = new Swiper(".home_container", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    loop: true,
+});
